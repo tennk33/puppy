2d893725b80d4036e7b21cfcd2068e55515b4d59
github tennk33 <github tennk33@gmail.com>
2021-12-14 19:13:45 +0900

    deploy

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ppppplease</h1>
+      <h2>hell no</h2>
+      <img src="./logo192.png" alt="p" />
+    </div>
+  );
+}
+
+export default App;
